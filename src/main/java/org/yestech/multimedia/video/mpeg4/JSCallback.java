409e2e3fc1d356ece9b6b5791232bbaf0708e9e2
Artie Copeland <artie@yestech.org>
2009-11-09 22:09:27 -0800

    reintegrated JSObject for applet figured out how to add the maven dependency

--- src/main/java/org/yestech/multimedia/video/mpeg4/JSCallback.java
+++ src/main/java/org/yestech/multimedia/video/mpeg4/JSCallback.java
@@ -9,7 +9,7 @@ package org.yestech.multimedia.video.mpeg4;
 
 import java.applet.Applet;
 
-//import sun.plugin.javascript.JSObject;
+import sun.plugin.javascript.JSObject;
 
 /**
  * JSCallback
@@ -19,36 +19,36 @@ public final class JSCallback {
 	private static boolean js_error = false;
 	
 	public static void startCallBack(Applet applet) {
-//		if(!js_error) {
-//			try {
-//				JSObject win = (JSObject) JSObject.getWindow(applet);
-//				win.call("framepump_start", null);
-//			} catch (Throwable tr) {
-//				js_error = true;
-//			}
-//		}
+		if(!js_error) {
+			try {
+				JSObject win = (JSObject) JSObject.getWindow(applet);
+				win.call("framepump_start", null);
+			} catch (Throwable tr) {
+				js_error = true;
+			}
+		}
 	}
 
 	public static void readyToPlayCallBack(Applet applet) {
-//		if(!js_error) {
-//			try {
-//				JSObject win = (JSObject) JSObject.getWindow(applet);
-//				win.call("framepump_ready", null);
-//			} catch (Throwable tr) {
-//				js_error = true;
-//			}
-//		}
+		if(!js_error) {
+			try {
+				JSObject win = (JSObject) JSObject.getWindow(applet);
+				win.call("framepump_ready", null);
+			} catch (Throwable tr) {
+				js_error = true;
+			}
+		}
 	}
 
 	public static void stopPlayCallBack(Applet applet) {
-//		if(!js_error) {
-//			try {
-//				JSObject win = (JSObject) JSObject.getWindow(applet);
-//				win.call("framepump_stop", null);
-//			} catch (Throwable tr) {
-//				js_error = true;
-//			}
-//		}
+		if(!js_error) {
+			try {
+				JSObject win = (JSObject) JSObject.getWindow(applet);
+				win.call("framepump_stop", null);
+			} catch (Throwable tr) {
+				js_error = true;
+			}
+		}
 	}
 
 }
